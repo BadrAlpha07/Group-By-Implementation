595ff2269dc92745f3ec00e70b34a173eec71fc9
Nadir Abdou <nadir.abdou@telecom-paristech.fr>
2020-10-24 12:27:59 +0200

    Create a class for input file and begenning of class to create a hashTable

---
+++ nested_loops/src/InputFile.java
@@ -0,0 +1,55 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+ 
+public class InputFile {
+	private BufferedReader reader = null;
+	
+	//Constructeur 
+	public InputFile(String file_name) {
+		try {
+			reader = new BufferedReader(new FileReader(file_name));
+			String line = reader.readLine();
+			while (line != null) {
+				System.out.println(line);
+				// read next line
+				line = reader.readLine();
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Return the BufferReader 
+	public BufferedReader getReader () {
+		return reader ;
+	}
+	
+	//Return one line from the file
+	public String readLine() {
+		BufferedReader reader_ = getReader();
+		try {	
+			String line = reader_.readLine();
+			if(line != null) {
+				return line; 
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null; 
+	}
+	
+	//Fermer le fichier 
+	public void closeFile() {
+		BufferedReader reader_ = getReader();
+		try {
+			reader_.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
