14e4fcb370475fd8567b4e76abf8742001fa44c6
Ulysse Demay <u.demay@gmail.com>
2020-11-02 18:51:59 +0100

    modification to avoid using reduceByKey

--- nested_loops/src/NestedLoopsSpark.java
+++ nested_loops/src/NestedLoopsSpark.java
@@ -41,13 +41,16 @@ public class NestedLoopsSpark {
     	
     	// Apply the nested loops algorithm with early aggregation to each partition of the JavaRDD
     	JavaRDD<Map.Entry<String, Integer>> hashtableRDD = csvLines.mapPartitions(x -> earlyAggregation(x).iterator());
-        
-    	// Transform the JavaRDD containing the output HashTable of each partition to a JavaPairRDD and merge these HashTables
-    	JavaPairRDD<String, Integer> keyValuePairs = hashtableRDD.mapToPair(obj -> new Tuple2(obj.getKey(), obj.getValue()));
-    	JavaPairRDD<String, Integer> partitionsMerged = keyValuePairs.reduceByKey((a,b) -> (a + b));
+    	
+    	/* Gather the partitions and merge the intermediary output hashtables.
+    	 * Note that we haven't used a reduce function. To replace it we have gathered the partitions into one partition and then applied
+    	 * the mapPartition function to all the entries.
+    	 * We have done this in order to only use nested loops algorithms for the GROUP BY and avoid the reduceByKey function provided by Spark.
+    	 */
+    	JavaRDD<Map.Entry<String, Integer>> mergedOutputsRDD = hashtableRDD.coalesce(1).mapPartitions(x -> mergeIntermediaryOutputs(x).iterator());
     	
     	// Write the result of the GROUP BY on an output CSV file
-    	JavaRDD<String> resultRDD = partitionsMerged.map(obj -> obj._1 + ";" + Integer.toString(obj._2));
+    	JavaRDD<String> resultRDD = mergedOutputsRDD.map(obj -> obj.getKey() + ";" + Integer.toString(obj.getValue()));
     	resultRDD.foreach((String line) -> {
     		output.writeLine(line);
     	});    	
@@ -56,7 +59,22 @@ public class NestedLoopsSpark {
     	this.sc.stop();
     }
     
-    /* Map function applying the nested loops algorithm with early aggregation to the partition given.
+    // Map function applying the nested loops algorithm to merge the intermediary output hashtables produced by each partition.
+    public static Iterable<Map.Entry<String, Integer>> mergeIntermediaryOutputs(Iterator<Map.Entry<String, Integer>> x) {
+    	Hashtable<String, Integer> outputTable = new Hashtable<String, Integer>();
+ 		while(x.hasNext()) {
+ 			Map.Entry<String, Integer> obj = x.next();
+ 	    	if (outputTable.containsKey(obj.getKey())) {
+ 	    		outputTable.put(obj.getKey(), outputTable.get(obj.getKey()) + obj.getValue());
+ 			} else {
+ 				outputTable.put(obj.getKey(), obj.getValue());
+ 			}
+ 		}
+ 		Set<Map.Entry<String, Integer>> iterableOutputTable = outputTable.entrySet();
+     	return iterableOutputTable;
+    }
+    
+    /* Map function applying the nested loops algorithm to the partition given.
      * Return a hashtable containing the result of the aggregation of the partition as an iterable object.
      */
  	public static Iterable<Map.Entry<String, Integer>> earlyAggregation(Iterator<String[]> x) {
